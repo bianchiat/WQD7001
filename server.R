80f7f84f9cd0ebda3b6fb12afa3e9288614ed7ac
bianchiat <38897664+bianchiat@users.noreply.github.com>
2019-05-26 17:14:59 +0800

    Add files via upload

---
+++ server.R
@@ -0,0 +1,72 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(leaflet)
+library(RColorBrewer)
+library(scales)
+library(lattice)
+library(dplyr)
+library(rlang)
+
+
+# Define server logic required to draw a histogram
+function(input, output, session) {
+  #dftable <- reactive({dff2[order(input$typeofdisease ,na.first=TRUE),]  })
+  
+ 
+  
+  target_quo = reactive ({
+    parse_quosure(input$typeofdisease)
+  })
+    
+ dftable<-  reactive ({
+    dff2%>%
+      arrange(desc(!!target_quo()))
+  })
+ 
+ dfmap<-reactive ({
+   dff2%>%
+     select(input$typeofdisease)
+ })
+  
+  
+  
+  ## Interactive Map ###########################################
+  
+  # Create the map
+  output$map <- renderLeaflet({
+    leaflet(geojson) %>% addTiles(urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+                                  attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>')%>%
+      addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 0.5,
+                  label = paste(dff$Country, ":", dfmap()[,1]),
+                  #label = paste(dff$Country, ":", (dff2$TB_Death_HIVneg_per100k_pop)),
+                  #color = pal(log10(dff2$TB_Death_HIVneg_per100k_pop))
+                  #color = pal(dff2$TB_Death_HIVneg_per100k_pop)
+                  #color = pal(dfmap()[,1])
+                  #color = pal(log10(dfmap()[,1]))
+                  color = pal(rescale(dfmap()[,1],na.rm=TRUE))
+                  #rescale(dff2$Confirm_cases_RR.MDR, na.rm = TRUE)
+                  
+                  
+      )%>%
+      setView(lng = 0, lat = 40, zoom = 2) %>%
+      addLegend("bottomleft",pal = pal, 
+               # values =log10(dff2$TB_Death_HIVneg_per100k_pop), opacity = 0.5)
+                 values =c(0:1), opacity = 0.5)
+  })
+  
+output$data <- renderTable({
+  head((dftable()[, c("Country", input$typeofdisease), drop = FALSE]) ,10)
+}, rownames = TRUE)
+
+#output$data <- renderDataTable(dftable())
+  #head((dftable()[, c("Country", input$typeofdisease), drop = FALSE]),10)
+  #output$data <- renderDataTable(dftable())
+}
---
+++ ui.R
@@ -0,0 +1,71 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+#install.packages("jsonlite")
+#install.packages("geojsonio")
+#install.packages("dplyr")
+#install.packages("leaflet")
+#install.packages("shiny")
+#install.packages("RColorBrewer")
+#install.packages("scales")
+#install.packages("lattice")
+#install.packages("DT")
+library(jsonlite)
+library(geojsonio)
+library(dplyr) 
+library(leaflet)
+library(shiny)
+library(RColorBrewer)
+library(scales)
+library(lattice)
+library(DT)
+
+vars <- names(dff2)
+vars <-vars[-1:-4]
+
+
+#vars <- c(
+# "Treat Succ-New Cases" = "Treat Succ-New Cases",
+# "Death" = "TB_Death_HIVminusper100k_pop"
+#)
+
+#vars2 <- c("2014","2015")
+
+# Define UI for application that draws a histogram
+navbarPage("TB", id="nav",
+           
+           tabPanel("Interactive Map",
+                    div(class="outer",
+                        
+                        tags$head(
+                          # Include our custom CSS
+                          includeCSS("styles.css"),
+                          includeScript("gomap.js")
+                        ),
+                        
+                        
+                        # If not using custom CSS, set height of leafletOutput to a number instead of percent
+                        
+                        leafletOutput("map", width="80%", height="100%"),
+                        
+                        
+                        
+                        # Shiny versions prior to 0.11 should use class = "modal" instead.
+                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                      draggable = FALSE, top = 55, left = "auto", right = 10, bottom = "auto",
+                                      width = 400, height = "100%",
+                                      
+                                      h2("TB data Explorer"),
+                                      
+                                      selectInput("typeofdisease", "Select the type of disease", vars),
+                                      
+                                      #dataTableOutput(outputId="data")
+                                      
+                                     tableOutput("data")
+                        ))))
